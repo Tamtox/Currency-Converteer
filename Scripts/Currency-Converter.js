7e82996f566496b61d00bcbacdd99904c99b3888
Tamtox <39272747@protonmail.com>
2021-02-04 10:11:02 +0600

    Logic Start

--- Scripts/Currency-Converter.js
+++ Scripts/Currency-Converter.js
@@ -1,3 +1,34 @@
-import codes from './Currency-Codes.js'
+import codes from './Currency-Codes.js';
 
-console.log(codes)
+// Variables
+const header = document.querySelector('#header');
+const main = document.querySelector('main');
+const convert = document.querySelector('#convert');
+const currency1 = document.querySelector('#currency1');
+const currency2 = document.querySelector('#currency2');
+const currency1Amount = document.querySelector('#currency1-amount');
+const swap = document.querySelector('#swap');
+
+// Events
+swap.addEventListener('click',function() {
+    [currency1.selectedIndex,currency2.selectedIndex] = [currency2.selectedIndex,currency1.selectedIndex]
+})
+convert.addEventListener('submit',function(e) {
+    e.preventDefault()
+})
+
+// Functions
+const functions = {
+    generateCodes(parent) {
+        const currencyCodes = Object.keys(codes);
+        for(let i=0;i<currencyCodes.length;i++) {
+            let currency = document.createElement('option');
+            currency.setAttribute('value',currencyCodes[i])
+            currency.innerText = currencyCodes[i];
+            parent.appendChild(currency);
+        }       
+    }
+}
+// Generate Codes
+functions.generateCodes(currency1);
+functions.generateCodes(currency2);
---
+++ Scripts/Functions.js
@@ -0,0 +1,4 @@
+const functions = {
+    
+}
+export default functions;
